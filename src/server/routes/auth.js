887c7c8dd8c2aca370e584019ff1cb6559492ca8
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-05-09 19:12:34 -0600

    login working

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,10 +2,10 @@ require('dotenv').load()
 var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
-
 var moment = require('moment');
 var jwt = require('jwt-simple');
 var queries = require('../db/queries.js');
+var bcrypt = require('bcrypt');
 
 router.post('/register', function(req, res, next) {
 // ensure user does not already exist
@@ -21,6 +21,7 @@ router.post('/register', function(req, res, next) {
             });
         }
         // create new user
+        req.body.password = hashing(req.body.password);
         queries.NewUser(req.body)
         .then(function(newUser) {
             newUser = newUser[0]
@@ -41,13 +42,46 @@ router.post('/register', function(req, res, next) {
     })
 });
 
-//
-//     });
-//   })
-//   .catch(function (err) {
-//     return next(err);
-//   });
-// });
+router.post('/login', function (req, res, next) {
+    // ensure that user exists
+    queries.Users().where('username', req.body.username)
+    .then(function (user) {
+        user = user[0];
+        if (!user) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'Username/password combination not found. Try again.'
+            });
+        }
+        if (comparePassword(req.body.password, user.password)) {
+            console.log("yay!");
+            var token = generateToken(user);
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    token: token,
+                    id: user.id,
+                    user: user.username
+                }
+            });
+        } else {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'Username/password combination incorrect. Try again.'
+            });
+        }
+    })
+    .catch(function (err) {
+        return next(err);
+    });
+});
+
+
+
+
+
+
+
 
 
 // // ** helpers ** //
@@ -62,45 +96,23 @@ function generateToken(user) {
   return jwt.encode(payload, process.env.TOKEN_SECRET);
 }
 
-// // ensure authenticated
-// function ensureAuthenticated(req, res, next) {
-//   // check headers for the presence of an auth object
-//   if(!(req.headers && req.headers.authorization)) {
-//     return res.status(400).json({
-//       status: 'fail',
-//       message: 'No header present or no authorization header.'
-//     });
-//   }
-//   // decode the token
-//   var header = req.headers.authorization.split(' ');
-//   var token = header[1];
-//   var payload = jwt.decode(token, process.env.TOKEN_SECRET);
-//   var now = moment().unix();
-//   // ensure that it is valid
-//   if(now > payload.exp || payload.iat > now) {
-//     return res.status(401).json({
-//       status: 'fail',
-//       message: 'Token is invalid'
-//     });
-//   }
-//   // ensure user is still in the database
-//   User.findById(payload.sub, function(err, user){
-//     if(err) {
-//       return next(err);
-//     }
-//     if(!user) {
-//       return res.status(400).json({
-//         status: 'fail',
-//         message: 'User does not exist'
-//       });
-//     }
-//     // attach user to request object
-//     req.user = user;
-//     // call next middleware function
-//     next();
-//   });
-// }
 
+function comparePassword(password, done)  {
+  bcrypt.compare(password, this.password, function(err, match) {
+    if(err) {
+      return done(err);
+    }
+    done(err, match);
+  });
+};
+
+function hashing(password) {
+  return bcrypt.hashSync(password, 10);
+}
+
+function comparePassword(password, hashedPassword) {
+  return bcrypt.compareSync(password, hashedPassword);
+}
 
 
 module.exports = router;
